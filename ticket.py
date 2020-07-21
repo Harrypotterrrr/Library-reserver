cb61e7087f347ba50a73958bcf3f95710069ca72
harrypotterrrr <jiahaolin19971119@126.com>
2020-07-21 21:26:03 +0800

    support multiple reserved list creating tickets

--- ticket.py
+++ ticket.py
@@ -11,15 +11,42 @@ class Ticket():
 
     def __init__(self, file_path, identity):
 
-        self.__load_params(file_path)
-        self.params["identity"] = identity
+        self.__load_params(file_path, identity)
 
-        img_name = f'{self.params["Date"]}{self.params["Session"][:2]}.png'
+        for payload in self.params:
+            self.create_ticket(payload)
 
-        ticket_path = self.ticket_path + f'/{img_name}' # original ticket
-        cticket_path = self.ticket_path + f'/d_{img_name}' # compressed ticket
+    def __load_params(self, file_path, identity):
 
-        self.w, self.h = 900, 1100
+        with open(file_path, "r", encoding='utf-8') as f:
+            config = json.load(f)
+
+        assert config["reply"] == "ok"
+
+        self.params = []
+
+        for i in range(len(config["list"])):
+            tmp_dict = {}
+            tmp_dict["id"] = config["list"][i]["id"]
+            tmp_dict["order"] = config["list"][i]["order"]
+            tmp_dict["libname"] = config["list"][i]["libname"]
+            tmp_dict["Date"] = config["list"][i]["Date"]
+            tmp_dict["Session"] = config["list"][i]["Session"]
+            tmp_dict["Locationid"] = config["list"][i]["Locationid"]
+            tmp_dict["note"] = config["list"][i]["note"]
+            tmp_dict["time"] = config["list"][i]["time"]
+            tmp_dict["identity"] = identity
+
+            self.params.append(tmp_dict)
+
+    def create_ticket(self, payload):
+
+        img_name = f'{payload["Date"]}{payload["Session"][:2]}.png'
+
+        ticket_path = self.ticket_path + f'/{img_name}'  # original ticket
+        cticket_path = self.ticket_path + f'/d_{img_name}'  # compressed ticket
+
+        self.w, self.h = 900, 1050
         self.interval_y1, self.interval_y2 = 70, 50
 
         self.font_type = './Deng.ttf'
@@ -32,10 +59,9 @@ class Ticket():
         self.wallpaper = Image.new("RGB", (self.w, self.h), "white")
         self.draw = ImageDraw.Draw(self.wallpaper)
 
-        self.draw_header()
+        self.draw_header(payload)
         self.draw_qrcode(self.create_qrcode())
-        self.draw_content()
-
+        self.draw_content(payload)
 
         self.wallpaper.save(ticket_path, 'png')
 
@@ -45,26 +71,9 @@ class Ticket():
 
         cp.print_success(f"Successfully create library ticket for {img_name}")
 
-    def __load_params(self, file_path, k = 0):
+    def draw_header(self, payload):
 
-        with open(file_path, "r", encoding='utf-8') as f:
-            config = json.load(f)
-
-        assert config["reply"] == "ok"
-
-        self.params = {}
-        self.params["id"] = config["list"][k]["id"]
-        self.params["order"] = config["list"][k]["order"]
-        self.params["libname"] = config["list"][k]["libname"]
-        self.params["Date"] = config["list"][k]["Date"]
-        self.params["Session"] = config["list"][k]["Session"]
-        self.params["Locationid"] = config["list"][k]["Locationid"]
-        self.params["note"] = config["list"][k]["note"]
-        self.params["time"] = config["list"][k]["time"]
-
-    def draw_header(self):
-
-        header = f'订单号：{self.params["order"]}'
+        header = f'订单号：{payload["order"]}'
         status = "状态 预约成功"
 
         header_w, header_h = self.draw.textsize(header, font=self.font1)
@@ -79,29 +88,29 @@ class Ticket():
     def draw_qrcode(self, img):
 
         img_w, img_h = img.size
-        qrcode_x, qrcode_y = (self.w - img_w) // 2, 250
+        qrcode_x, qrcode_y = (self.w - img_w) // 2, 200
 
         self.wallpaper.paste(img, (qrcode_x, qrcode_y))
 
-    def draw_content(self):
+    def draw_content(self, payload):
 
-        datetime = f'{self.params["Date"]} {self.params["Session"]}'
-        location = f'进馆序号：{self.params["Locationid"]}'
+        datetime = f'{payload["Date"]} {payload["Session"]}'
+        location = f'进馆序号：{payload["Locationid"]}'
 
-        libname_w, libname_h = self.draw.textsize(self.params["libname"], font=self.font1)
+        libname_w, libname_h = self.draw.textsize(payload["libname"], font=self.font1)
         datetime_w, datetime_h = self.draw.textsize(datetime, font=self.font2)
         location_w, location_h = self.draw.textsize(location, font=self.font2)
-        indentity_w, identity_h = self.draw.textsize(self.params["identity"], font=self.font2)
+        indentity_w, identity_h = self.draw.textsize(payload["identity"], font=self.font2)
 
-        libname_x, libname_y = (self.w - libname_w) // 2, 820
+        libname_x, libname_y = (self.w - libname_w) // 2, 770
         datetime_x, datetime_y = (self.w - datetime_w) // 2, libname_y + self.interval_y1
         location_x, location_y = (self.w - location_w) // 2, datetime_y + self.interval_y2
         indentity_x, identity_y = (self.w - indentity_w) // 2, location_y + self.interval_y2
 
-        self.draw.text((libname_x, libname_y), self.params["libname"], self.color, self.font1)
+        self.draw.text((libname_x, libname_y), payload["libname"], self.color, self.font1)
         self.draw.text((datetime_x, datetime_y), datetime, self.color, self.font2)
         self.draw.text((location_x, location_y), location, self.color, self.font2)
-        self.draw.text((indentity_x, identity_y), self.params["identity"], self.color, self.font2)
+        self.draw.text((indentity_x, identity_y), payload["identity"], self.color, self.font2)
 
     def create_qrcode(self):
 
